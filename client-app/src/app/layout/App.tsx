d36817db0075f30122469be2e369a6187909ed93
Harsh Bothra <harsh.love.619@gmail.com>
2023-07-12 02:32:46 +0530

    Added ActivityDashboard

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Container, List, ListItem } from 'semantic-ui-react';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  useEffect(() => {
+    axios.get<Activity[]>("https://localhost:7204/api/activities")
+    .then(response => {
+      setActivities(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <NavBar />
+      <Container style={{marginTop:'7em'}}>
+        <ActivityDashboard activities = {activities} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
