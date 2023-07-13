28c0650e50280aefdc487937e4a5977b87636393
Harsh Bothra <harsh.love.619@gmail.com>
2023-07-14 01:26:15 +0530

    Local Crud operations in UI

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Container, List, ListItem } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import { v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -34,6 +35,17 @@ function App() {
     setEditMode(false)
   }
 
+  function handleCreateOrEdit(activity: Activity){
+    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+    :setActivities([...activities, {...activity, id:uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function hadleDeleteActivity(id: string){
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
+
   return (
     <>
       <NavBar openForm={handleFormOpen} />
@@ -46,6 +58,8 @@ function App() {
           editMode = {editMode}
           openForm = {handleFormOpen}
           closeForm = {handleFormClose}
+          createOrEdit = {handleCreateOrEdit}
+          deleteActivity = {hadleDeleteActivity}
         />
       </Container>
     </>
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,10 +5,11 @@ import { Activity } from '../../../app/models/activity';
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void
 }
 
 
-export default function ActivityList({ activities, selectActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
     return (
         <Segment>
             <Item.Group divided>
@@ -24,6 +25,8 @@ export default function ActivityList({ activities, selectActivity }: Props) {
                             <Item.Extra>
                                 <Button floated='right' content='View' color='blue' 
                                     onClick={() => selectActivity(activity.id)} />
+                                <Button floated='right' content='Delete' color='red' 
+                                    onClick={() => deleteActivity(activity.id)} />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
